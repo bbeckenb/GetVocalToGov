d3c6e168b2c0805039f3bbe485d192a47807d90c
Bryce Beckenbach <brycebeckenbach@gmail.com>
2021-12-08 20:54:29 -0500

    completed CRUD routes and testing for User resource

--- routes/users.js
+++ routes/users.js
@@ -2,19 +2,44 @@
 
 const express = require("express");
 const jsonschema = require("jsonschema");
-const { ensureLoggedIn } = require("../../jobly-backend/middleware/auth");
+const userUpdateSchema = require("../schemas/userUpdateSchema.json");
 const {
     checkLoggedIn,
     checkCorrectUserOrAdmin
 } = require("../middleware/auth");
 const User = require("../models/User");
+const { BadRequestError } = require("../ExpressError");
 
 const router = express.Router();
 
-router.get("/:username", ensureLoggedIn, async function (req, res, next) {
+router.get("/:username", checkLoggedIn, async function (req, res, next) {
     try {
-        const user = await User.get(req.params.username);
+        const user = await User.getUser(req.params.username);
         return res.json({ user });
+    } catch (err) {
+        return next(err);
+    }
+});
+
+router.patch("/:username", checkCorrectUserOrAdmin, async function (req, res, next) {
+    try {
+        const validator = jsonschema.validate(req.body, userUpdateSchema);
+        if(!validator.valid) {
+            const errs = validator.errors.map(err => err.stack);
+            throw new BadRequestError(errs);
+        }
+        const userBeforeChanges = await User.getUser(req.params.username);
+        const user = await User.update(req.params.username, { ...req.body, isAdmin: userBeforeChanges.isAdmin });
+        return res.json({ user })
+    } catch (err) {
+        return next(err);
+    }
+});
+
+router.delete("/:username", checkCorrectUserOrAdmin, async function (req, res, next) {
+    try {
+        await User.deleteUser(req.params.username);
+        return res.json({ deleted: req.params.username });
     } catch (err) {
         return next(err);
     }
--- routes/users.test.js
+++ routes/users.test.js
@@ -14,6 +14,8 @@ const {
     commonBeforeEach,
     commonAfterEach,
     commonAfterAll,
+    testUser0TokenAdmin,
+    testUser1TokenNonAdmin
 } = require('./_testCommon');
 
 beforeAll(commonBeforeAll);
@@ -24,12 +26,140 @@ afterAll(commonAfterAll);
 describe("GET /users/:username", function () {
     test("works", async function () {
         const res = await request(app)
-            .post("/auth/token")
-            .send({ username: "JDean1", password: "1234" });
+            .get("/users/JDean1")
+            .set("authorization", `Bearer ${testUser1TokenNonAdmin}`);
+        
         expect(res.body).toEqual({
-            "token": expect.any(String)
+            user: {
+                id: expect.any(Number),
+                firstName: 'Jimmy',
+                lastName: 'Dean',
+                username: 'JDean1',
+                password: expect.any(String),
+                address: 'my house',
+                email: 'jdean@gmail.com',
+                isAdmin: true
+              }
         });
+        expect(res.statusCode).toEqual(200);
+    });
+
+    test("sends unauth err if requestor doesn't have a valid token", async function () {
+        const res = await request(app)
+            .get("/users/JDean1");
+        
+        expect(res.statusCode).toEqual(401);
+    });
+
+    test("sends not found err if username not in db", async function () {
+        const res = await request(app)
+            .get("/users/wrongo")
+            .set("authorization", `Bearer ${testUser1TokenNonAdmin}`);
+        
+        expect(res.statusCode).toEqual(404);
     });
 });
 
-    
+describe("PATCH /users/:username", function () {
+    test("works", async function () {
+        const res = await request(app)
+            .patch("/users/JDean1")
+            .send({
+                id: expect.any(Number),
+                firstName: 'Jammy',
+                lastName: 'Dane',
+                username: 'JDean12',
+                password: '1234',
+                address: 'your house',
+                email: 'jdean1@gmail.com',              
+            })
+            .set("authorization", `Bearer ${testUser0TokenAdmin}`);
+
+        expect(res.body).toEqual({
+            user: {
+                id: expect.any(Number),
+                password: expect.any(String),
+                firstName: 'Jammy',
+                lastName: 'Dane',
+                username: 'JDean12',
+                address: 'your house',
+                email: 'jdean1@gmail.com', 
+                isAdmin: true             
+              }
+        });
+        
+        expect(res.statusCode).toEqual(200);
+    });
+
+    test("sends unauth err if requestor doesn't have a valid token", async function () {
+        const res = await request(app)
+            .patch("/users/JDean1");
+        
+        expect(res.statusCode).toEqual(401);
+    });
+
+    test("sends unauth err if not same user or admin", async function () {
+        const res = await request(app)
+            .patch("/users/JDean1")
+            .send({
+                id: expect.any(Number),
+                firstName: 'Jammy',
+                lastName: 'Dane',
+                username: 'JDean12',
+                password: '1234',
+                address: 'your house',
+                email: 'jdean1@gmail.com',              
+            })
+            .set("authorization", `Bearer ${testUser1TokenNonAdmin}`);
+        
+        expect(res.statusCode).toEqual(401);
+    });
+
+    test("sends bad request err if data doesn't match jsonschema", async function () {
+        const res = await request(app)
+            .patch("/users/JDean1")
+            .send({
+                id: expect.any(Number),
+                password: '1234',
+                address: 'your house',
+                email: 'jdean1@gmail.com',              
+            })
+            .set("authorization", `Bearer ${testUser0TokenAdmin}`);
+        
+        expect(res.statusCode).toEqual(400);
+    });
+});
+
+describe("DELETE /users/:username", function () {
+    test("works", async function () {
+        const res = await request(app)
+            .delete("/users/JDean1")
+            .set("authorization", `Bearer ${testUser0TokenAdmin}`);
+
+        expect(res.body).toEqual({ deleted: 'JDean1' });
+        expect(res.statusCode).toEqual(200);
+    });
+
+    test("sends unauth err if token does not match requested username for deletion", async function () {
+        const res = await request(app)
+            .delete("/users/JDean1")
+            .set("authorization", `Bearer ${testUser1TokenNonAdmin}`);
+
+        expect(res.statusCode).toEqual(401);
+    });
+
+    test("sends not found err if username DNE token does not match requested username for deletion", async function () {
+        const res = await request(app)
+            .delete("/users/wrongo")
+            .set("authorization", `Bearer ${testUser0TokenAdmin}`);
+
+        expect(res.statusCode).toEqual(404);
+    });
+
+    test("sends unauth if no token", async function () {
+        const res = await request(app)
+            .delete("/users/JDean1")
+            
+        expect(res.statusCode).toEqual(401);
+    });
+});
